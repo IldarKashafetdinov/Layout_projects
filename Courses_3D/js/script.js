e43988b07fd9d1e3aafc9a5678f6610adc068a8b
ildar <ildar.kashafetdinov@gmail.com>
2023-05-10 13:29:48 +0300

    first comm

---
+++ Courses_3D/js/script.js
@@ -0,0 +1,29 @@
+
+
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.nav__list');
+let menuLinks = menu.querySelectorAll('.nav__link');
+
+
+
+burger.addEventListener('click', function(){
+
+  burger.classList.toggle('burger--active');
+
+  menu.classList.toggle('nav__list--active');
+
+  document.body.classList.toggle('stop-scroll');
+})
+
+
+
+menuLinks.forEach(function(element) {
+  element.addEventListener('click', function() {
+
+    burger.classList.remove('burger--active');
+
+    menu.classList.remove('nav__list--active');
+
+    document.body.classList.remove('stop-scroll');
+  })
+})
